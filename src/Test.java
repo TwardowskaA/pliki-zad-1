d91e2fec003f8b183fba9f6f4a0a8e46ada3f47e
Agata <twardowska123@gmail.com>
2018-08-02 19:46:13 +0200

    zadanie

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+
+        Product[] product = new Product[3];
+        product[0] = new Product("Mleko","Mlekowita", 2.5);
+        product[1] = new Product("Woda","Staropolanka", 1.7);
+        product[2] = new Product("Telewizor","Samsung", 2300);
+
+
+        FileWriter fw = new FileWriter("product.csv");
+        BufferedWriter bfw = new BufferedWriter(fw);
+        for (int i = 0; i <3 ; i++) {
+
+        bfw.write(product[i].getName());
+        bfw.write(";");
+        bfw.write(product[i].getProducer());
+        bfw.write(";");
+        bfw.write(String.valueOf(product[i].getPrice()));
+        bfw.newLine();
+        }
+        bfw.close();
+
+    }
+}
